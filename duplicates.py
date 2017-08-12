472921e422537819723227519cd72767a9143b0f
DM45 <mda.tomsk@gmail.com>
2017-08-12 20:38:39 +0700

    Решение задачи

--- duplicates.py
+++ duplicates.py
@@ -1,3 +1,19 @@
+import os
+
+
+def get_duplicate_files(directory):
+    filename_list = []
+    duplicate_file = []
+    for root, directories, files in os.walk(directory):
+        for filename in files:
+            if filename in filename_list:
+                duplicate_file.append(filename)
+            else:
+                filename_list.append(filename)
+    return ' '.join(set(duplicate_file))
+
 
 if __name__ == '__main__':
-    pass
+    _filepath = input('Enter directorys path for check: ')
+    print('Duplicate files: ')
+    print(get_filepaths(_filepath))
